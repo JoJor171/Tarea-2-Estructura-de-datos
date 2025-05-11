5d0a03943aad6552336072e273a5eb90d66c9872
Your Name <you@example.com>
2025-05-11 08:27:51 -0400

    Resolviendo conflictos de merge

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define LIMITE_CANCIONES 113998
 
 typedef struct {
     char id[100];
@@ -41,7 +42,7 @@ int is_equal_str(void *key1, void *key2) {
 
   void mostrarCancion(Song *song) {
     printf("ID: %s\n", song->id);
-    printf("Título: %s\n", song->title);
+    printf("Titulo: %s\n", song->title);
     printf("Artista: %s\n", song->artist);
     printf("Tempo: %.1f BPM\n", song->tempo);
     printf("Generos: ");
@@ -70,47 +71,92 @@ int is_equal_str(void *key1, void *key2) {
     }
 }
 
+Song* crear_cancion(char **campos) {
+  Song *cancion = (Song *)malloc(sizeof(Song));
+  if (cancion == NULL) {
+      perror("Error al asignar memoria para la canción");
+      exit(1);
+  }
 
-/**
- * Carga las canciones desde un archivo CSV y las almacena en un mapa por ID.
- */
+  strcpy(cancion->id, campos[0]);
+  strcpy(cancion->title, campos[4]);
+  strcpy(cancion->artist, campos[2]);
+  cancion->genres = split_string(campos[20], ";");
+  cancion->tempo = atof(campos[18]);
+
+  return cancion;
+}
+
 void cargar_canciones(Map *canciones_byid, Map *canciones_bygenre, Map *canciones_byartist) {
-    // Intenta abrir el archivo CSV que contiene datos de películas
+  long cuantas_cargar = 0;
+
+  printf("Canciones que desea cargar (MAX. 113998): ");
+  scanf("%ld", &cuantas_cargar);
+
   FILE *archivo = fopen("data/song_dataset_.csv", "r");
   if (archivo == NULL) {
-    perror(
-        "Error al abrir el archivo"); // Informa si el archivo no puede abrirse
-    return;
+      perror("ERROR: No se pudo abrir el archivo");
+      return;
   }
 
   char **campos;
-  // Leer y parsear una línea del archivo CSV. La función devuelve un array de
-  // strings, donde cada elemento representa un campo de la línea CSV procesada.
-  campos = leer_linea_csv(archivo, ','); // Lee los encabezados del CSV
+  campos = leer_linea_csv(archivo, ','); // Saltar encabezado
 
-  int k=0;
-  // Lee cada línea del archivo CSV hasta el final
+  long k = 0;
   while ((campos = leer_linea_csv(archivo, ',')) != NULL) {
-    k++;
-    if (k>10) break; //carga primeras 1000 canciones
-    printf("ID: %d\n", atoi(campos[0]));
-    printf("Titulo cancion: %s\n", campos[4]);
+      if (k >= cuantas_cargar) break;
 
-    List* artistas = split_string(campos[2], ";");
+      Song *nueva_cancion = (Song *)malloc(sizeof(Song));
+      if (nueva_cancion == NULL) {
+          perror("ERROR: No se pudo asignar memoria para la canción");
+          fclose(archivo);
+          return;
+      }
 
-    printf("Artistas: \n");
-    for(char *artista = list_first(artistas); artista != NULL; 
-                    artista = list_next(artistas))
-        printf("  %s\n", artista);
+      strncpy(nueva_cancion->id, campos[0], sizeof(nueva_cancion->id));
+      strncpy(nueva_cancion->artist, campos[2], sizeof(nueva_cancion->artist));
+      strncpy(nueva_cancion->title, campos[4], sizeof(nueva_cancion->title));
+      nueva_cancion->tempo = atof(campos[18]);
+      nueva_cancion->genres = split_string(campos[20], ";");
 
-    printf("Album: %s\n", campos[3]);
-    printf("Genero: %s\n", campos[20]);
-    printf("Tempo: %.2f\n", atof(campos[18]));
-    printf(" -------------------------------\n");
-    
+      map_insert(canciones_byid, nueva_cancion->id, nueva_cancion);
+
+      for (char *genero = list_first(nueva_cancion->genres); genero != NULL; genero = list_next(nueva_cancion->genres)) {
+          MapPair *par_genero = map_search(canciones_bygenre, genero);
+          if (par_genero == NULL) {
+              List *lista_genero = list_create();
+              list_pushBack(lista_genero, nueva_cancion);
+              map_insert(canciones_bygenre, genero, lista_genero);
+          } else {
+              List *lista_genero = (List *)par_genero->value;
+              list_pushBack(lista_genero, nueva_cancion);
+          }
+      }
+
+      List *artistas = split_string(nueva_cancion->artist, ";");
+      for (char *artista = list_first(artistas); artista != NULL; artista = list_next(artistas)) {
+          MapPair *par_artista = map_search(canciones_byartist, artista);
+          if (par_artista == NULL) {
+              List *lista_artista = list_create();
+              list_pushBack(lista_artista, nueva_cancion);
+              map_insert(canciones_byartist, artista, lista_artista);
+          } else {
+              List *lista_artista = (List *)par_artista->value;
+              list_pushBack(lista_artista, nueva_cancion);
+          }
+      }
+      list_clean(artistas); 
+
+      if (k == 1) printf("Cargando canciones...\n");
+
+      k++;
   }
-  fclose(archivo); // Cierra el archivo después de leer todas las líneas
+
+  fclose(archivo);
+  printf("Canciones cargadas exitosamente.\n");
+  printf("Se han cargado %ld canciones.\n", k);
 }
+
 
 void crear_lista_reproduccion(Map *listas_reproduccion) {
     char nombre[100];
@@ -156,6 +202,7 @@ void agregar_cancion_a_lista(Map *canciones_byid, Map *listas_reproduccion) {
 void buscar_por_artista(Map *canciones_byartist) {
     char artista[200];
     printf("Ingrese el nombre del artista: ");
+    getchar();
     scanf("%[^\n]", artista);
   
     MapPair *pair = map_search(canciones_byartist, artista);
@@ -216,7 +263,7 @@ void buscar_por_tempo(Map *canciones_byid) {
     }
   }
 
-int main() {
+  int main() {
     char opcion;
   
     Map *canciones_byid = map_create(is_equal_str);
@@ -225,37 +272,45 @@ int main() {
     Map *listas_reproduccion = map_create(is_equal_str);
   
     do {
-      mostrarMenuPrincipal();
-      printf("Ingrese una opcion: ");
-      scanf(" %c", &opcion);
+        mostrarMenuPrincipal();
+        printf("Ingrese una opcion: ");
+        scanf(" %c", &opcion);
   
-      switch (opcion) {
-      case '1':
-        cargar_canciones(canciones_byid, canciones_bygenre, canciones_byartist);
-        break;
-      case '2':
-        buscar_por_genero(canciones_bygenre);
-        break;
-      case '3':
-        buscar_por_artista(canciones_byartist);
-        break;
-      case '4':
-        buscar_por_tempo(canciones_byid);
-        break;
-      case '5':
-        crear_lista_reproduccion(listas_reproduccion);
-        break;
-      case '6':
-        agregar_cancion_a_lista(canciones_byid, listas_reproduccion);
-        break;
-      case '7':
-        mostrar_canciones_lista(listas_reproduccion);
-        break;
+        switch (opcion) {
+        case '1':
+            cargar_canciones(canciones_byid, canciones_bygenre, canciones_byartist);
+            break;
+        case '2':
+            buscar_por_genero(canciones_bygenre);
+            break;
+        case '3':
+            buscar_por_artista(canciones_byartist);
+            break;
+        case '4':
+            buscar_por_tempo(canciones_byid);
+            break;
+        case '5':
+            crear_lista_reproduccion(listas_reproduccion);
+            break;
+        case '6':
+            agregar_cancion_a_lista(canciones_byid, listas_reproduccion);
+            break;
+        case '7':
+            mostrar_canciones_lista(listas_reproduccion);
+            break;
+        case '8':
+            printf("Saliendo del programa...\n");
+            break;
+        default:
+            printf("Opción no válida. Intente nuevamente.\n");
+            break;
+        }
+
+        if (opcion != '8') {
+            presioneTeclaParaContinuar();
+        }
+
+    } while (opcion != '8');
   
-      //if (opcion != '8') presioneTeclaParaContinuar();
-    }
-      } while (opcion != '8');
-    
-      return 0;
-  
+    return 0;
 }
